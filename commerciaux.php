06e106f08783a3d0523d48365fafcdab734c7215
Nicola42 <nicola.carlo.15@gmail.com>
2021-01-28 09:36:24 +0100

    form

--- commerciaux.php
+++ commerciaux.php
@@ -7,7 +7,17 @@
 </head>
 <body>
     <form action="commerciaux.php" method="post">
-        
+        <div>
+            <label for="nom_commerciaux">Nom :</label>
+            <input type="text" id="nom_commerciaux" name="nom-commerciaux">
+        </div>
+        <div>
+            <label for="date_inscription">Date :</label>
+            <input type="date" id="date_inscription" name="date_inscription">
+        </div>
+        <div>
+            <input type="submit" value="Send">
+        </div>
     </form>
 </body>
 </html>
