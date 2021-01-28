a5b209b7e4ae9e2554a024b75e5bd63d99e18769
Nicola42 <nicola.carlo.15@gmail.com>
2021-01-28 09:40:07 +0100

    title & link css

--- produits.php
+++ produits.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="style_produits.css">
+    <title>Produits</title>
 </head>
 <body>
     <form action="produits.php" method="post">
